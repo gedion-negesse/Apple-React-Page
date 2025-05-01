846dc6bc68fdb03cf5e9fc93bc1a02dcd1ddfa85
gedion <legessegedion@gmail.com>
2025-05-01 17:19:30 -0400

    redirecting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Apple-React-Page",
+  base: "/Apple-React-Page/",
   plugins: [react()],
 });
